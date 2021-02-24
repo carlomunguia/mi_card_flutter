a106bc62909c16ff3d989fe492d060b4e5365f35
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-02-24 07:19:59 -0600

    Mi Card: Containers
    working through manipulating containers in dart/flutter world

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.indigo,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.white,
+                  child: Text('Container 1')),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blueGrey,
+                child: Text('Container 2'),
+              ),
+              SizedBox(height: 20.00),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.amber,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
